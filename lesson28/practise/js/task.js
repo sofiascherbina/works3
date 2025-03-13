167eeafe5d7d8bf4052d52868f4b6c42a47ba3a7
Софія Щербина <160267180+sofiascherbina@users.noreply.github.com>
2025-03-13 19:50:30 +0200

    done tasks

--- lesson28/practise/js/task.js
+++ lesson28/practise/js/task.js
@@ -1,18 +1,18 @@
-const players = [
-    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
-    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
-    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
-    { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-    { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
-  ];
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+//   ];
 
-console.log(players.map(item => item.name));
-console.log(players.map(item => item.points + item.points*0.1));
-let newTime = players.find(item => item.id === 'player-3')
+// console.log(players.map(item => item.name));
+// console.log(players.map(item => item.points + item.points*0.1));
+// let newTime = players.find(item => item.id === 'player-3')
 // let members = players.map(player => player.id === 'player-3'? );
 
-let updated = {...newTime, timePlayed: newTime.timePlayed = 540}
-console.log(updated);
+// let updated = {...newTime, timePlayed: newTime.timePlayed = 540}
+// console.log(updated);
 
 // let newList = players.find((item) => {
 //     if(item.id = 'player-3'){
@@ -22,10 +22,10 @@ console.log(updated);
 // })
 // console.log(newList);
 
-console.log(players.filter(item => item.online === true)); 
-console.log(players.filter(item => item.online === false));
-console.log(players.filter(item => item.timePlayed > 250));
-console.log(players.find(item => item.id = prompt('enter id')));
-console.log(players.find(item => item.name = prompt('enter name')));
-console.log(players.every(item => item.timePlayed > 200 ));
-console.log(players.every(item => item.online === true)); 
+// console.log(players.filter(item => item.online === true)); 
+// console.log(players.filter(item => item.online === false));
+// console.log(players.filter(item => item.timePlayed > 250));
+// console.log(players.find(item => item.id = prompt('enter id')));
+// console.log(players.find(item => item.name = prompt('enter name')));
+// console.log(players.every(item => item.timePlayed > 200 ));
+// console.log(players.every(item => item.online)); 
